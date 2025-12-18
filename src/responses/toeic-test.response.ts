8932960111822ceefb2e260e97827fefb600335c
Nguyett11 <buianhnguyet08072003@gmail.com>
2025-12-18 13:32:06 +0700

    chore(reponse progress detail)

--- src/responses/toeic-test.response.ts
+++ src/responses/toeic-test.response.ts
@@ -1,12 +1,16 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
 
 export class ToeicTestResponse {
   @ApiProperty()
+  @Expose()
   id: number;
 
   @ApiProperty()
+  @Expose()
   title: string;
 
   @ApiProperty()
+  @Expose()
   description: string | null;
 }
