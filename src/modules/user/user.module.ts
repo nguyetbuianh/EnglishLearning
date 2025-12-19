96d047d8c07fa5edc705efb6512fca980aa15ad0
Thiều Quang Thắng <thang.thieuquang@ncc.asia>
2025-12-19 16:24:48 +0700

    feat: add api user profile

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,16 +3,24 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "../../entities/user.entity";
 import { UserService } from "./user.service";
 import { ToeicModule } from "../toeic/toeic.module";
+import { UserController } from "./user.controller";
+import { StatService } from "../stat/stat.service";
+import { UserStats } from "../../entities/user-stat.entity";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([User,
+      UserStats
+    ]),
   ],
+  controllers: [UserController],
   providers: [
-    UserService
+    UserService,
+    StatService
   ],
   exports: [
-    UserService
+    UserService,
+    StatService,
   ],
 })
 export class UserModule { }
